f95533e43b7514b7f98de9c8c59c807f43994227
Tristan Gates <56215102+tristan-gates@users.noreply.github.com>
2021-03-04 17:48:22 -0600

    Started files

---
+++ app/src/main/java/edu/jsu/mcis/cs408/lab4/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.jsu.mcis.cs408.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
